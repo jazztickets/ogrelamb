8007e5649c210edb097a5c0df19487f69791ef10
Alan Witkowski <alan.witkowski@gmail.com>
2013-04-07 22:50:04 -0600

    worked on pch for linux

--- src/character.h
+++ src/character.h
@@ -15,7 +15,7 @@
 *	You should have received a copy of the GNU General Public License
 *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************************/
-#include "stdafx.h"
+#include <all.h>
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
--- src/objectmanager.h
+++ src/objectmanager.h
@@ -15,7 +15,7 @@
 *	You should have received a copy of the GNU General Public License
 *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************************/
-#include "stdafx.h"
+#include <all.h>
 #ifndef OBJECTMANAGER_H
 #define OBJECTMANAGER_H
 
